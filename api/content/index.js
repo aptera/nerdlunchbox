5500f8abc0c4d8f591f3cd17af3f07c1f64a50ae
Jon Fazzaro <JFazzaro@apterainc.com>
2021-01-21 13:03:42 -0500

    clean

--- api/content/index.js
+++ api/content/index.js
@@ -1,12 +1,24 @@
 const fetch = require("node-fetch");
+const querystring = require('querystring');
 
 module.exports = async function (context, req) {
-    const query = `https://slack.com/api/conversations.history?token=${process.env["SLACK_API_TOKEN"]}&channel=${process.env["SLACK_CHANNEL_ID"]}&oldest=1604076099.014800`;
-
-    const result = await fetch(query)
+    const result = await fetch(url())
         .then(res => res.json());
 
     context.res = {
         body: result
     };
+
+    function url() {
+        const resource = "https://slack.com/api/conversations.history";
+        return [resource, params()].join("?");
+    }
+
+    function params() {
+        return {
+            token: process.env["SLACK_API_TOKEN"],
+            channel: process.env["SLACK_CHANNEL_ID"],
+            oldest: process.env["QUERY_SINCE"]
+        };
+    }
 }
