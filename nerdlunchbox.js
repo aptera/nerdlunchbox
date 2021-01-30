07b6d8ce7c0b380edd935a477ade612ee7e7d5b9
Adam Swanson <adam.swanson.98@gmail.com>
2021-01-30 10:14:17 -0500

    clean: move styles and frontend JS to separate files

---
+++ nerdlunchbox.js
@@ -0,0 +1,55 @@
+console.log("Loading...");
+const result = fetch("/api/content")
+    .then(res => res.json())
+    .then(result => {
+        display(result);
+    });
+
+function display(submissions) {
+    const list = document.getElementById("results");
+    list.innerHTML = "";
+    submissions.forEach(addItemTo(list));
+}
+
+function addItemTo(list) {
+    return r => {
+        const a = document.createElement("a");
+        a.innerHTML = render(r.content);
+        a.href = r.url;
+        addVotes(a, r);
+
+        const li = document.createElement("li");
+        li.appendChild(a);
+        list.appendChild(li);
+    };
+}
+
+function addVotes(to, r) {
+    const votes = div("votes");
+    votes.appendChild(div("upvotes", `&uarr;${r.upvotes}`));
+    votes.appendChild(div("downvotes", `&darr;${r.downvotes}`));
+    to.appendChild(votes);
+}
+
+function div(className, content) {
+    const div = document.createElement("div");
+    div.className = className;
+    div.innerHTML = content || "";
+    return div;
+}
+
+function render(content) {
+    return marked(
+        decodeBlockquotes(
+            removeAutoLinks(content)), {
+            breaks: true
+        });
+}
+
+function decodeBlockquotes(content) {
+    return content.replace(/&gt;+/g, '>');
+}
+
+function removeAutoLinks(content) {
+    return content.replace(/<http:\/\/.+\|(.+)>/g, '$1');
+}
